6de62333972c68a133faef0d0aed978d19dc9e4e
Jakub Kozłowski <kubukoz@gmail.com>
2019-01-12 20:43:42 +0100

    Update expected target file

--- generator/src/test/resources/target-coproducts-scala-models.scala
+++ generator/src/test/resources/target-coproducts-scala-models.scala
@@ -1,7 +1,6 @@
 package models
 
 final case class Cat(huntingSkill: Option[Cat.HuntingSkill])
-
 object Cat {
   sealed abstract class HuntingSkill(value: String) extends Product with Serializable
 
@@ -13,9 +12,32 @@ object Cat {
   }
 }
 
+sealed trait Dog extends Product with Serializable
+
+object Dog {
+  final case class Husky(husky: models.Husky) extends Dog
+  final case class York(york: models.York) extends Dog
+  final case class Anonymous$1(name: String, age: Double, gender: Option[Anonymous1.Gender]) extends Dog
+  object Anonymous$1 {
+    sealed abstract class Gender(value: String) extends Product with Serializable
+
+    object Gender {
+      case object Female extends Gender("female")
+      case object Male extends Gender("male")
+    }
+  }
+}
+
 final case class Dog2(value: Dog)
 
 final case class Husky(woof: Option[String])
+
+sealed trait Pet extends Product with Serializable
+
+object Pet {
+  final case class Cat(cat: models.Cat) extends Pet
+  final case class Dog(dog: models.Dog) extends Pet
+}
 
 sealed trait Strnum extends Product with Serializable
 
@@ -25,26 +47,3 @@ object Strnum {
 }
 
 final case class York(woof: Option[String])
-
-sealed trait Pet extends Product with Serializable
-
-object Pet {
-  case class Cat(cat: models.Cat) extends Pet
-  case class Dog(dog: models.Dog) extends Pet
-}
-
-sealed trait Dog extends Product with Serializable
-
-object Dog {
-  case class Husky(husky: models.Husky) extends Dog
-  case class York(york: models.York) extends Dog
-  final case class Anonymous$1(name: String, age: Double, gender: Option[Anonymous1.Gender]) extends Dog
-
-  object Anonymous$1 {
-    sealed abstract class Gender(value: String) extends Product with Serializable
-    object Gender {
-      case object Female extends Gender("female")
-      case object Male extends Gender("male")
-    }
-  }
-}
